4a186cb699791f87335898bb3754532200e82e15
Никита <81251222+NikitaKelevra@users.noreply.github.com>
2022-05-22 20:56:34 +0300

    Initial Commit

---
+++ Строитель - TableViewCellBuilder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Строитель - TableViewCellBuilder
+//
+//  Created by Сперанский Никита on 22.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
